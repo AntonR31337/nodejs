4679351440becf72525e6b3a09b0644100dfd58a
Anton Ryabtsev <anton_ryabcev@mail.ru>
2022-05-11 21:00:23 +0300

    i've wrote the checker

--- lesson1/index.js
+++ lesson1/index.js
@@ -1,3 +1,18 @@
-console.log(progress.argv);
-const [name] = progress.argv.slice(2);
-console.log('Hello Node.js to ' + name);
+import colors from "colors";
+
+// console.log(process.argv);
+const [min, max] = process.argv.slice(2);
+Number(min, max);
+
+// console.log(colors.green(`Hello Node.js to ${vasy} and ${petya}`));
+
+const a = Number(min);
+const b = Number(max);
+
+if ((a % a == 0) && ( a > 0)){
+    for (let i = a; i <= b; i++ ) {
+        console.log(colors.red(i));
+    };
+} else {
+    console.log("Вы ввели неверное число");
+}
